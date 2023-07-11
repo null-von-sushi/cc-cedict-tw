be5da07f0db2dd4de6a46b6f3520a000dc87716f
null <null-von-sushi@null.ssl-mail.com>
2023-07-11 21:23:41 +0100

    Update script.sh

--- tools/script.sh
+++ tools/script.sh
@@ -5,16 +5,24 @@ database_file="/full/path/to/cedict_ts.u8"
 
 # Read the file line by line
 while IFS= read -r line; do
-    # Assign the values to variables
+    # Skip comment lines starting with '#'
+    if [[ $line =~ ^#.* ]]; then
+        continue
+    fi
+
+    # Extract fields from the line
     TRADITIONAL=$(echo "$line" | awk -F ' ' '{print $1}')
     SIMPLIFIED=$(echo "$line" | awk -F ' ' '{print $2}')
-    TRANSCRIPTION=$(echo "$line" | awk -F ' ' '{print $3}')
-    DEFINITION=$(echo "$line" | awk -F '/' '{print $2}')
+    TRANSCRIPTION=$(echo "$line" | grep -o '\[.*\]' | tr -d '[]')
+    DEFINITIONS=$(echo "$line" | awk -F '/' '{for (i=2; i<=NF; i++) print $i}')
 
-    # Print the variables (you can modify this part as per your requirement)
+    # Print the variables with multiple definitions on separate lines
     echo "Traditional: $TRADITIONAL"
     echo "Simplified: $SIMPLIFIED"
     echo "Transcription: $TRANSCRIPTION"
-    echo "Definition: $DEFINITION"
+    echo "Definitions:"
+    while IFS= read -r definition; do
+        echo "$definition"
+    done <<< "$DEFINITIONS"
     echo "------------------------------"
 done < "$database_file"
