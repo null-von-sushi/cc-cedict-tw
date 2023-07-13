36744e9ffa59f6645bbfa9d6df45dcbbf8ff94cc
null <null-von-sushi@null.ssl-mail.com>
2023-07-13 20:50:57 +0100

    Made search + suggest work
    It now seems to search properly, as well as suggest the correct changes.
    
    Just need to implement saving back to the file.

--- tools/patch.sh
+++ tools/patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the database
-database="/home/sysop/Documents/GitHub/cc-cedict-tw/tools/tmp/cc-cedict-tw.u8"
+database="/home/sysop/Documents/GitHub/cc-cedict-tw/tools/tmp/cc-cedict-tw_mini.u8"
 
 # Iterate over each line in the database
 while IFS= read -r line; do
@@ -12,7 +12,25 @@ while IFS= read -r line; do
   temp_full=""
   temp_hanzi=""
   temp_pinyin=""
-  temp_full=$(echo "$DEFINITIONS" | awk -F '|' 'match($0, /[^\x00-\x7F]+\[([^\]]+)\]/) {print substr($0, RSTART, RLENGTH)}')
+
+  # try to extract both if possible
+  temp_full=$(echo "$DEFINITIONS" | awk -F '|' 'match($0, /([A-Za-z0-9]|[^\x00-\x7F])+\|([A-Za-z0-9]|[^\x00-\x7F])+/) {print substr($0, RSTART, RLENGTH)}')
+  # If no traditional specifically is not available, fall back to alternative method which just gets any characters
+  if [[ -z "$temp_full" ]]; then
+    temp_full=$(echo "$DEFINITIONS" | awk -F '|' 'match($0, /[^\x00-\x7F]+\[([^\]]+)\]/) {print substr($0, RSTART, RLENGTH)}')
+  # if both are available, use both
+  else
+    # get traditional
+    temp_full_a=$(echo $temp_full | perl -nle 'print $& if /([A-Za-z0-9]|[^\x00-\x7F])+\|/')
+    #remove `|`
+    temp_full_a=${temp_full_a%"|"}
+    #get simplified
+    temp_full_b=$(echo $temp_full | perl -nle 'print $& if /\|([A-Za-z0-9]|[^\x00-\x7F])+/')
+    #remove `|`
+    temp_full_b=${temp_full_b#"|"}
+    temp_full=$(echo "$temp_full_a $temp_full_b")
+  fi
+
   temp_pinyin=$(echo "$temp_full" | awk 'match($0, /\[([^\]]+)\]/) {print substr($0, RSTART, RLENGTH)}')
   temp_hanzi=$(echo "$temp_full" | awk 'match($0, /[^\x00-\x7F]+/) {print substr($0, RSTART, RLENGTH)}')
 
@@ -20,22 +38,38 @@ while IFS= read -r line; do
   if [ -n "$temp_hanzi" ]; then
 
     # search database for matching entry and extract it into variable
-    temp_dictionaryentry=$(grep "$temp_hanzi \[" "$database")
+    temp_dictionaryentry=$(grep "$temp_hanzi \[" "$database" | grep "PRC pr. \[")
     if [ -n "$(echo "$temp_dictionaryentry" | grep "PRC pr. \[")" ]; then
       if [ -n "$temp_dictionaryentry" ]; then
-        # mostly debug output
-        echo "Reference found: $temp_full"
-        echo "Original Pinyin: $temp_pinyin"
-        echo "Hanzi (Simplified): $temp_hanzi"
-        echo "Relevant dictionary entry/entries: $temp_dictionaryentry"
-        echo "Line is: $line"
-        # Construct the new line with the desired format
-        temp_new_pinyin=$(perl -ne "print if /[^\x00-\x7F]+\\s\Q$temp_hanzi\E\\s\\[([^\\]]+)\\]/" $database | awk -F '[[]' '{print $2}' | awk -F ']' '{print $1}')
-        temp_new_hanzi="$temp_hanz"
-        temp_new="[$temp_new_pinyin]"
-        new_line=$(echo "$line" | perl -pe 's/\Q'"$temp_full"'\E/'"$temp_new"'/g')
 
-        echo "New line will be: $new_line"
+        # get pinyin of dictionary entry
+        temp_dictionaryentry_pinyin=$(echo $temp_dictionaryentry | awk -F '[[]' '{print $2}' | awk -F ']' '{print $1}')
+
+        # If the pronounciation is already the same, no need to do anything
+        if [ "$temp_pinyin" = "[$temp_dictionaryentry_pinyin]" ]; then
+          # echo "The variables have the same value."
+          true
+        else
+          # Check if the two words are even the same. We don't want [xi1] to randomly turn into [ge4 ben1 dong1 xi1]...
+          if [ "$(echo "$temp_dictionaryentry" | awk -F ' ' '{print $1}')" = "$temp_hanzi" ]; then
+            # # Debug
+            # echo -e "Reference found for\n\"$temp_full\"\nFound in \n\"$line\""
+            # echo -e "Reference consists of the following data:\nHanzi (Simplified): \n$temp_hanzi\nPinyin: \n$temp_pinyin"
+            # echo -e "Relevant dictionary entry/entries found in database: \n$temp_dictionaryentry"
+            # echo "Conclusion: $temp_pinyin => [$temp_dictionaryentry_pinyin]"
+
+            echo "Line was: $line"
+            # Construct the new line with the desired format
+            temp_new_pinyin=$(perl -ne "print if /[^\x00-\x7F]+\\s\Q$temp_hanzi\E\\s\\[([^\\]]+)\\]/" $database | awk -F '[[]' '{print $2}' | awk -F ']' '{print $1}')
+            temp_new_hanzi="$temp_hanz"
+            temp_new="[$temp_new_pinyin]"
+            new_line=$(echo "$line" | perl -pe 's/\Q'"$temp_full"'\E/'"$temp_new"'/g')
+            echo -e "$temp_full"
+            echo "Line will be: $new_line"
+
+          fi
+
+        fi
 
       else
 
