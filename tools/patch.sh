966c625104253ccfb211250d91ed3cda5935fde1
null <null-von-sushi@null.ssl-mail.com>
2023-07-15 22:09:29 +0100

    Add blacklist Nr. 1
    Added a blacklist function so that certain words can be blacklisted

--- tools/patch.sh
+++ tools/patch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Path to the database
-database="/home/sysop/Documents/GitHub/cc-cedict-tw/tools/tmp/cc-cedict-tw_mini.u8"
+database="/home/sysop/Documents/GitHub/cc-cedict-tw/tools/tmp/cc-cedict-tw.u8"
+databaseblacklist="$database.blacklist"
 
 # Iterate over each line in the database
 while IFS= read -r line; do
@@ -55,7 +56,7 @@ while IFS= read -r line; do
             # # Debug
             # echo -e "Reference found for\n\"$temp_full\"\nFound in \n\"$line\""
             # echo -e "Reference consists of the following data:\nHanzi (Simplified): \n$temp_hanzi\nPinyin: \n$temp_pinyin"
-            # echo -e "Relevant dictionary entry/entries found in database: \n$temp_dictionaryentry"
+            echo -e "Relevant dictionary entry/entries found in database: \n$temp_dictionaryentry"
             # echo "Conclusion: $temp_pinyin => [$temp_dictionaryentry_pinyin]"
 
             echo "Line was: $line"
@@ -64,8 +65,41 @@ while IFS= read -r line; do
             temp_new_hanzi="$temp_hanz"
             temp_new="[$temp_new_pinyin]"
             new_line=$(echo "$line" | perl -pe 's/\Q'"$temp_full"'\E/'"$temp_new"'/g')
-            echo -e "$temp_full"
+            echo -e "Temp full $temp_full"
             echo "Line will be: $new_line"
+
+            search_string=$temp_full
+            # Escape brackets in search_string
+            escaped_search_string=$(echo "$search_string" | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g')
+
+            # Get index of part we need to replace
+            index=$(echo "$line" | awk -v search="$escaped_search_string" 'BEGIN{FS="/"} {for (i=1; i<=NF; i++) if (index($i, search) > 0) print i-1}' 2>/dev/null)
+            # No idea why this is needed, but it is
+            ((index = index + 1))
+            # make it a new variable
+            temp_replace=$(echo "$line" | awk -v idx="$index" 'BEGIN{FS="/"} {if (idx >= 0 && idx < NF) print $idx}')
+            echo "searching for in \"$search_string\" (index $index) in \"$temp_full\". Escaped search string is: \"$escaped_search_string\""
+            echo "$temp_replace"
+
+            # make it so it will only proceed if we're still matching the right text.
+            # all barely works and it's better to skip entries than to have a wrong/broken DB
+            # so: check if text we're about to modify includes the text we're searching for to begin with
+            if [[ $temp_replace == *"$temp_full"* ]]; then
+              echo "yes"
+              echo -e "$temp_full"
+              echo "$temp_replace"
+
+              # Load a file with a blacklist of things to skip since we don't know how to work around some issues
+              # right now, and will this be manually speifying some things to skip
+              if ! grep -qF "$temp_full" "$databaseblacklist"; then
+                echo "Can continue with $temp_full"
+              else
+                echo "Skipping $temp_full due to blacklist"
+              fi
+
+            else
+              echo "no"
+            fi
 
           fi
 
